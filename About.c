b4f0b3058554d08c74768f9c26a494c60f3cd69c
yeargin2021 <88904457+yeargin2021@users.noreply.github.com>
2021-11-16 13:38:08 -0500

    Create About.c
    Added rules to the game.

---
+++ About.c
@@ -0,0 +1,7 @@
+int main(void) {
+  /* 
+  This is a game popularized on the CBS sitcom THE BIG BANG THEORY.
+  Sheldon explains how the game works: "Scissors cuts paper, paper covers rock, rock crushes lizard, lizard poisons Spock, Spock smashes scissors, scissors decapitates lizard, lizard eats paper, paper disproves Spock, Spock vaporizes rock, and as it always has, rock crushes scissors."
+  This project is a work in progress. Enjoy!
+  */
+}
