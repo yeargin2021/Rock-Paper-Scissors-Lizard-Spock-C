8b91dd97fb68e6c2ab26f37a4849a4364ee84124
yeargin2021 <88904457+yeargin2021@users.noreply.github.com>
2022-03-30 17:00:13 -0400

    Some changes to the JS version...

--- js/File01.js
+++ js/File01.js
@@ -1,1 +1,7 @@
 //document.write("Hello there.\nI'm Tommy.");
+
+
+
+// START...This demonstrates the JS random number generator.
+document.getElementById("demo").innerHTML = Math.random();
+// END...This demonstrates the JS random number generator.
